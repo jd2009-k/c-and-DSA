bf8bb989f3395f0f81965090e8ded65cb0d68195
jd2009 <jaydonga23@gmail.com>
2026-09-04 16:19:23 +0530

    c++

---
+++ lec-1/bynarisort/amzon.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    int num;
+    cin >> num;
+    int lo = 0, hi = 1, mid;
+    for (; v[hi] <= num;)
+    {
+        lo = hi;
+        hi = hi * 2;
+    }
+    for (; lo <= hi;)
+    {
+        mid = (long long int)(lo + hi) / 2;
+        if (v[mid] == num)
+        {
+            break;
+        }
+        if (v[mid] > num)
+        {
+            hi = mid - 1;
+        }
+        if (v[mid] < num)
+        {
+            lo = mid + 1;
+        }
+    }
+    cout << mid;
+}
