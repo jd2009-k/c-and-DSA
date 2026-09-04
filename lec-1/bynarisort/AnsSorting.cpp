4f725d4fffe6295c18a228774d8db0d0cdc53e64
jd2009 <jaydonga23@gmail.com>
2026-09-05 01:49:11 +0530

    Ans Sorting

---
+++ lec-1/bynarisort/AnsSorting.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, h, max = 0, sum = 0;
+    cin >> n >> h;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        sum += v[i];
+        if (max < v[i])
+            max = v[i];
+    }
+    long long int lo = 1, hi = max;
+    for (; lo <= hi;)
+    {
+        mid = (hi + lo) / 2;
+        int sum1 = 0;
+        for (; sum != sum1;)
+    }
+}
