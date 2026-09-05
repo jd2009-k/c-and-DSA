354c7429944d40043129b5c16478027b5ae1e2dc
jd2009 <jaydonga23@gmail.com>
2026-09-05 23:53:22 +0530

    m

--- lec-1/bynarisort/AnsSorting.cpp
+++ lec-1/bynarisort/AnsSorting.cpp
@@ -4,6 +4,7 @@ int main()
 {
     int n, h, max = 0, sum = 0;
     cin >> n >> h;
+    vector<int> v(n);
     for (int i = 0; i < n; i++)
     {
         cin >> v[i];
@@ -11,11 +12,26 @@ int main()
         if (max < v[i])
             max = v[i];
     }
-    long long int lo = 1, hi = max;
+    long long int lo = 1, hi = max, mid;
     for (; lo <= hi;)
     {
         mid = (hi + lo) / 2;
-        int sum1 = 0;
-        for (; sum != sum1;)
+        if (lo == hi)
+            break;
+        int sum1 = 0, j = 0, hor = 0, b = 0;
+        for (; j < n; j++)
+        {
+            int hores = v[j] / mid;
+            hor += hores;
+            if (v[j] % mid > 0)
+                hor++;
+        }
+        if (hor < h)
+            hi = mid;
+        else if (h < hor)
+            lo = mid + 1;
+        else
+            break;
     }
+    return mid;
 }
